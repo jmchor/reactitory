ec61edfdf81e1653912c0e3ac7fa45b3206523c1
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-13 17:03:59 +0100

    update the search bar component

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -23,65 +23,74 @@ function SearchBar({ onSearch }) {
 
 		// Call the onSearch callback with the constructed query
 		onSearch(query);
+		setSearchTerm('');
+		setSearchOptions({
+			searchAlbum: false,
+			searchArtist: false,
+			searchTrack: false,
+			searchGenre: false,
+		});
 	};
 
 	return (
-		<div>
-			<input type='text' placeholder='Search...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-			<label>
-				Album
-				<input
-					type='checkbox'
-					checked={searchOptions.searchAlbum}
-					onChange={() =>
-						setSearchOptions((prevOptions) => ({
-							...prevOptions,
-							searchAlbum: !prevOptions.searchAlbum,
-						}))
-					}
-				/>
-			</label>
-			<label>
-				Artist
-				<input
-					type='checkbox'
-					checked={searchOptions.searchArtist}
-					onChange={() =>
-						setSearchOptions((prevOptions) => ({
-							...prevOptions,
-							searchArtist: !prevOptions.searchArtist,
-						}))
-					}
-				/>
-			</label>
-			<label>
-				Track
-				<input
-					type='checkbox'
-					checked={searchOptions.searchTrack}
-					onChange={() =>
-						setSearchOptions((prevOptions) => ({
-							...prevOptions,
-							searchTrack: !prevOptions.searchTrack,
-						}))
-					}
-				/>
-			</label>
-			<label>
-				Genre
-				<input
-					type='checkbox'
-					checked={searchOptions.searchGenre}
-					onChange={() =>
-						setSearchOptions((prevOptions) => ({
-							...prevOptions,
-							searchGenre: !prevOptions.searchGenre,
-						}))
-					}
-				/>
-			</label>
-			<button onClick={handleSearch}>Search</button>
-		</div>
+		<>
+			<div className='search-area'>
+				<div className='search-bar-container'>
+					<input
+						className='search-input'
+						type='text'
+						placeholder='Search...'
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+					<button className='search-button' onClick={handleSearch}>
+						Search
+					</button>
+				</div>
+				<div className='checkbox-options'>
+					<label className='checkbox-label'>
+						Album
+						<input
+							type='checkbox'
+							checked={searchOptions.searchAlbum}
+							onChange={() =>
+								setSearchOptions((prevOptions) => ({ ...prevOptions, searchAlbum: !prevOptions.searchAlbum }))
+							}
+						/>
+					</label>
+					<label className='checkbox-label'>
+						Artist
+						<input
+							type='checkbox'
+							checked={searchOptions.searchArtist}
+							onChange={() =>
+								setSearchOptions((prevOptions) => ({ ...prevOptions, searchArtist: !prevOptions.searchArtist }))
+							}
+						/>
+					</label>
+					<label className='checkbox-label'>
+						Track
+						<input
+							type='checkbox'
+							checked={searchOptions.searchTrack}
+							onChange={() =>
+								setSearchOptions((prevOptions) => ({ ...prevOptions, searchTrack: !prevOptions.searchTrack }))
+							}
+						/>
+					</label>
+					<label className='checkbox-label'>
+						Genre
+						<input
+							type='checkbox'
+							checked={searchOptions.searchGenre}
+							onChange={() =>
+								setSearchOptions((prevOptions) => ({ ...prevOptions, searchGenre: !prevOptions.searchGenre }))
+							}
+						/>
+					</label>
+				</div>
+			</div>
+		</>
 	);
 }
 
