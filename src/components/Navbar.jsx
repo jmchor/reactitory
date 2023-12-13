8c8acf4b795f20552d7bafb9c2fc3f5211b3a9a8
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-13 10:35:03 +0100

    Create basic navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Assuming you are using React Router
+
+function Navbar() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to='/search'>Search Database</Link>
+				</li>
+				<li>
+					<Link to='/new-records'>Insert New Records</Link>
+				</li>
+				<li>
+					<Link to='/alter-records'>Alter Record</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Navbar;
