3d36a1720fa1d552f63d4dd842b6d5cf55848588
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-13 10:36:46 +0100

    Delete unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import { useEffect } from 'react';
-import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
