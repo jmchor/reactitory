f71c7d4b71d1d411917131f1af87ef09f48fc4bf
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-15 15:23:33 +0100

    Add About page component

---
+++ src/pages/editPages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+	return (
+		<div>
+			Image by{' '}
+			<a href='https://www.freepik.com/free-vector/hand-drawn-black-speaker-background_4471596.htm#query=speaker&position=25&from_view=search&track=sph&uuid=708bf892-f84d-4cc1-9c9f-b06f420b2ae2'>
+				Freepik
+			</a>
+		</div>
+	);
+}
+
+export default About;
