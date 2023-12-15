6421aaf813b394c32ec0d47ca1911b6b82cd3015
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-15 15:22:59 +0100

    Update Home page layout and content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
-			<p>Home page content</p>
+		<div className='home-screen'>
+			<img className='home-image' src='/speaker.jpg' alt='' />
+			<div className='headline-container-home'>
+				<div className='headline-container' id='auditory'>
+					<h1>Auditory</h1>
+					<div className='backdrop-black'></div>
+				</div>
+				<div className='backdrop-big'></div>
+			</div>
+			<p>Look up artist, albums and tracks, create stuff, and be happy about it.</p>
 		</div>
 	);
 }
