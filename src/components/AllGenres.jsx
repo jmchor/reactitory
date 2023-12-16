00560d04b04242940ea4e129d268daacf0b7b96f
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-16 16:06:46 +0100

    Create AllGenres component

---
+++ src/components/AllGenres.jsx
@@ -0,0 +1,30 @@
+function AllGenres({ genres, searchTerm }) {
+	return (
+		<>
+			<div className='headline-container'>
+				<h2>All Genres (like, ALL of them!)</h2>
+				<div className='backdrop-smaller'></div>
+			</div>
+
+			{Array.isArray(genres) && genres.length > 0 ? (
+				<ul className='genres-table'>
+					{genres.map((genre, index) => (
+						<li key={genre} className='genre-item'>
+							<div className='headline-container'>
+								<p>{genre}</p>
+								<div className='backdrop-genres'></div>
+							</div>
+						</li>
+					))}
+				</ul>
+			) : (
+				<p>
+					{searchTerm} genre
+					{searchTerm.endsWith('s') ? '' : 's'}
+				</p>
+			)}
+		</>
+	);
+}
+
+export default AllGenres;
