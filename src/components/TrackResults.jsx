c79563657ff1909c33828602adef918a29b8d345
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-18 21:11:55 +0100

    Adjust trackResults component to receive array param

--- src/components/TrackResults.jsx
+++ src/components/TrackResults.jsx
@@ -1,33 +1,73 @@
-function TrackResults({ track }) {
-	return (
-		<div className='track-container'>
-			<div className='headline-container'>
-				<h2>{track.track}</h2>
-				<div className='backdrop-smaller'></div>
-			</div>
+import axios from 'axios';
+const SERVER = import.meta.env.VITE_API_URL;
 
-			<div className='tracks-table' id='for-tracks'>
-				<table>
-					<thead>
-						<tr>
-							<th>Album</th>
-							<th>Artist</th>
-							<th>Duration</th>
-							<th>YouTube URL</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>{track.album}</td>
-							<td>{track.artist}</td>
-							<td>{track.duration}</td>
-							<td>{track.youtube_url || '---'}</td>
-						</tr>
-					</tbody>
-				</table>
+function TrackResults({ tracks }) {
+	const handleButtonClick = async (trackId) => {
+		try {
+			// Make a POST request to update the YouTube URL for the specific track
+			const response = await axios.post(`${SERVER}/ytupdate/single-track/${trackId}`);
+
+			if (response.data.success) {
+				console.log('YouTube URL updated for track:', response.data.track);
+				window.history.go(-1);
+
+				setTimeout(() => {
+					window.history.go(1);
+				}, 1000);
+
+				// You can add any further logic here, such as updating the state
+			} else {
+				console.error('Failed to update YouTube URL:', response.data.error);
+			}
+		} catch (error) {
+			console.error('Error updating YouTube URL:', error.message);
+		}
+	};
+
+	if (Array.isArray(tracks)) {
+		return (
+			<div className='track-container'>
+				<div className='headline-container'>
+					<h2>Tracks</h2>
+					<div className='backdrop-smaller'></div>
+				</div>
+
+				<div className='tracks-table' id='for-tracks'>
+					<table>
+						<thead>
+							<tr>
+								<th>Album</th>
+								<th>Artist</th>
+								<th>Track</th>
+								<th>Duration</th>
+								<th>YouTube URL</th>
+							</tr>
+						</thead>
+						<tbody>
+							{tracks.map((track, index) => (
+								<tr key={index}>
+									<td>{track.album}</td>
+									<td>{track.artist}</td>
+									<td>{track.track}</td>
+									<td>{track.duration}</td>
+									<td>
+										{track.youtube_url ? (
+											<a href={track.youtube_url} target='_blank' rel='noopener noreferrer'>
+												Watch on YouTube
+											</a>
+										) : (
+											<button className='yt-button' onClick={() => handleButtonClick(track.track_id)}>
+												Add YouTube URL
+											</button>
+										)}
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
-
 export default TrackResults;
