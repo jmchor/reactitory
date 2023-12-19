01f9e93f3820b82463023fc7c35af4af09e8b6c4
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-19 09:41:38 +0100

    adjust navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,18 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-	const location = useLocation();
-
-	const handleRefresh = (pathname) => {
-		// Check if the current location pathname matches the link pathname
-		if (location.pathname === pathname) {
-			// Use window.location.reload() to refresh the current page
-			window.location.reload();
-		}
-	};
-
 	return (
 		<nav>
 			<ul>
 				<li>
-					<Link to='/' onClick={() => handleRefresh('/')}>
-						Home
-					</Link>
+					<a href='/'>Home</a>
 				</li>
 				<li>
-					<Link to='/new-records' onClick={() => handleRefresh('/new-records')}>
-						Insert New Records
-					</Link>
+					<a href='/new-records'>New DB Records</a>
 				</li>
 
 				<li>
-					<Link to='/about' onClick={() => handleRefresh('/about')}>
-						About
-					</Link>
+					<a href='/about'>About Auditory</a>
 				</li>
 			</ul>
 		</nav>
