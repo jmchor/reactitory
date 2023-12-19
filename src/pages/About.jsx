06a469057fd94cf8db4662f615234e09a0b27103
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-19 10:18:35 +0100

    Update about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,83 +1,53 @@
 function About() {
 	return (
-		<div>
-			<h1 id='reactitory-the-auditory-frontend-written-in-react'>
-				Reactitory - the auditory frontend written in React
-			</h1>
-			<p>
-				This is the frontend magic for the <a href='https://github.com/jmchor/auditory'>auditory backend magic</a>.
-				Magic all around.
-			</p>
-			<p align='center'>
-				<img src='public/reactitory.png' alt='the auditory fron page' width='500px' />
-			</p>
-			<h2 id='features'>Features</h2>
-			<p>All of the routes are getting called - and now you have a nice frontend UI for it!</p>
-			<ul>
-				<li>
-					<p>
-						<strong>Create New Records:</strong> Type in the name of an artist - and auditory does the rest (while you
-						get to enjoy loading spinners!)
-					</p>
-				</li>
-				<li>
-					<p>
-						<strong>Alter Records:</strong> Every Artist and Album has the option to edit the database entry
-					</p>
-				</li>
-				<li>
-					<p>
-						<strong>Dynamic Search Results:</strong> The component dynamically fetches and displays results based on the
-						user&#39;s search query, including artists, albums, tracks, and genres.
-					</p>
-				</li>
-				<li>
-					<p>
-						<strong>Pagination:</strong> For lengthy result lists, the component implements pagination to improve user
-						navigation and experience.
-					</p>
-				</li>
-				<li>
-					<p>
-						<strong>Modal Display:</strong> When a user clicks on an image in the table, a modal with a larger version
-						of the image is displayed.
-					</p>
-				</li>
-			</ul>
-			<h2 id='usage'>Usage</h2>
-			<p>Put in a search term, select a search option, and see what you get!</p>
-			<ul>
-				<li>
-					<p>
-						<strong>Track Search:</strong> Here you NEED to put in the ARTIST, TRACK names. I found that loads of
-						artists share track titles.
-					</p>
-				</li>
-				<li>
-					<p>
-						<strong>Album Search:</strong> Here you can choose - just the ALBUM, or ALBUM, ARTIST - if you&#39;re not
-						sure if the album name is unique or not.
-					</p>
-				</li>
-			</ul>
-			<h2 id='dependencies'>Dependencies</h2>
-			<ul>
-				<li>React</li>
-				<li>Axios</li>
-			</ul>
-			<h2 id='how-to-run'>How to Run</h2>
-			<ol>
-				<li>
-					Install dependencies: <code>npm install</code>
-				</li>
-				<li>
-					Run the application: <code>npm start</code>
-				</li>
-			</ol>
-			Image by{' '}
-			<a href='https://www.freepik.com/free-vector/hand-drawn-black-speaker-background_4471596.htm#query=speaker&position=25&from_view=search&track=sph&uuid=708bf892-f84d-4cc1-9c9f-b06f420b2ae2'>
-				Freepik
-			</a>
+		<div className='about-container'>
+			<div className='headline-container'>
+				<h1>Welcome and thank you for checking out Auditory!</h1>
+				<div className='backdrop'></div>
+			</div>
+
+			<div className='about-text'>
+				<h3>What is this?</h3>
+				<p>
+					Auditory is yet another "scratch my own itch" project, as developers with a job that doesn't involve
+					developing so often create.
+				</p>
+				<p>The itches involved here were:</p>
+				<ul>
+					<li>my React is getting rusty</li>
+					<li>my Node is getting rusty</li>
+					<li>my JavaScript overall is getting rusty</li>
+					<li>I have no idea what kind of music I have on my computer</li>
+					<li>maybe I could organize it in a database?</li>
+					<li>I need to practice with PostgreSQL</li>
+					<li>the Spotify API looks fun!</li>
+				</ul>
+				<p>&nbsp;</p>
+				<p>
+					And here we are. Written in <strong>Node.js</strong>, <strong>React</strong>, <strong>Express</strong> and{' '}
+					<strong>PostgreSQL</strong>, Auditory is a database application that will fetch data and store it in a
+					database for you - including manipulation!
+				</p>
+				<p>
+					So, basic CRUD of course - no authentication or anything, you're supposed to either run it on your own system
+					(localhost) or deploy it somewhere only you can access it - or share, if you want to.
+				</p>
+				<p>You can</p>
+				<ol>
+					<li>
+						look up data (like artists and albums by name, get more info on each, search for specific tracks using
+						artist AND trackname)
+					</li>
+					<li>edit data (change database entries right in the frontend UI)</li>
+					<li>the deletion of data is not yet part of the package. (So that would technically be a "can't").</li>
+				</ol>
+				<p>
+					Oh, and the image on the home page is by{' '}
+					<a href='https://www.freepik.com/free-vector/hand-drawn-black-speaker-background_4471596.htm#query=speaker&position=25&from_view=search&track=sph&uuid=708bf892-f84d-4cc1-9c9f-b06f420b2ae2'>
+						Freepik
+					</a>
+				</p>
+			</div>
 		</div>
 	);
 }
